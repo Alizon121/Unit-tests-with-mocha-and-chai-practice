1eaa133594bfc421353dbcdd92bbbb3b785ff74f
Andrew <andrew.lizon12@gmail.com>
2024-05-01 13:27:16 -0700

    Completed Step 3 in func module

--- funcs.js
+++ funcs.js
@@ -1,14 +1,33 @@
 function isFive(num) {
-  // Your code here 
+  if (num === 5) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function isOdd(number) {
-  // Your code here 
+
+if (typeof number !== "number") {
+  throw new Error("number should be a Number type")
+} else if (number % 2 !== 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here 
+  let arr = []
+  if (min > max) {
+    return []
+  } 
+ for (let i = min; i <= max; i+=step) {
+   arr.push(i)
+} 
+return arr
 }
+
 
 
 module.exports = { isFive, isOdd, myRange };
